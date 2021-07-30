87e4a5a58761a171e203faf5600a248423ff9ac4
Joesh Gichon <joeshgichon56@gmail.com>
2021-07-30 19:56:29 +0300

    This will separate user logic with backend logic in js file

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
         
         alert("Thank You" + " " + firstName + " " + lastName + " " + "for reaching you us.")
     });
+    (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true)
 });
 
 // frontend logic/UI/UX
@@ -63,4 +64,3 @@ $("document").ready(function(){
     });
 });
 
-(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true)
