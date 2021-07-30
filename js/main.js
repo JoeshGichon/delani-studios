e22ccfcf4b0e6fe2330bfb8dd4a77abbb42e9e4b
Joesh Gichon <joeshgichon56@gmail.com>
2021-07-30 16:21:47 +0300

    This will remove the redirect file and add a functionality that connects the form with mailchimp api which collects the submitted data and thanks the user for subscribing

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 // forms data collection
 
 $(document).ready(function(){
-    $("form#collect").submit(function(event){
+    $("form").submit(function(event){
         // preventing default form submission to itself
         event.preventDefault();
 
         // acquiring inputed values
-        let name = $("#name").val();
-        let email = $("#email").val();
+        let firstName = $("#mce-FNAME").val();
+        let lastName = $("#mce-LNAME").val();
+        let email = $("#mce-EMAIL").val();
         let comments = $("#comments").val();
-
-        alert("Thank You" + " " + name + " " + "for reaching you us.")
+        
+        alert("Thank You" + " " + firstName + " " + lastName + " " + "for reaching you us.")
     });
 });
 
@@ -60,11 +61,6 @@ $("document").ready(function(){
     },function(){
         $(".portfolio-items>div>img").removeClass("portFolio-hover");
     });
-    
 });
 
-
-
-
-
-
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true)
